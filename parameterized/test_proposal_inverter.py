4fa7a3211cf945f900bf3401deb2a38431d000b0
Austin Nhung <austinnhung@yahoo.ca>
2021-12-07 14:26:01 -0700

    Fix unit tests from bug fixes found during analysis

--- parameterized/test_proposal_inverter.py
+++ parameterized/test_proposal_inverter.py
@@ -50,7 +50,8 @@ def test_add_broker(inverter, broker1):
     # Add broker to proposal inverter
     broker1 = inverter.add_broker(broker1, 50)
 
-    assert inverter.funds == 550
+    assert inverter.funds == 500
+    assert inverter.stake == 50
     assert inverter.number_of_brokers() == 1
 
     assert broker1.funds == 50
@@ -69,7 +70,8 @@ def test_claim_broker_funds(inverter, broker1, broker2):
 
     broker1 = inverter.claim_broker_funds(broker1)
 
-    assert inverter.funds == 450
+    assert inverter.funds == 400
+    assert inverter.stake == 50
     assert broker1.funds == 150
 
     # Add a second broker
@@ -80,7 +82,8 @@ def test_claim_broker_funds(inverter, broker1, broker2):
 
     broker2 = inverter.claim_broker_funds(broker2)
 
-    assert inverter.funds == 450
+    assert inverter.funds == 350
+    assert inverter.stake == 100
     assert broker2.funds == 100
 
     # Make a claim after the minimum number of epochs
@@ -88,7 +91,8 @@ def test_claim_broker_funds(inverter, broker1, broker2):
 
     broker1 = inverter.claim_broker_funds(broker1)
 
-    assert inverter.funds == 350
+    assert inverter.funds == 250
+    assert inverter.stake == 100
     assert broker1.funds == 250
 
     
@@ -102,14 +106,17 @@ def test_remove_broker(inverter, broker1, broker2):
     broker2 = inverter.add_broker(broker2, 100)
 
     assert inverter.number_of_brokers() == 2
-    assert inverter.funds == 700
+    assert inverter.funds == 500
+    assert inverter.stake == 200
 
     inverter.iter_epoch(20)
 
+    # Remove a broker before the minimum number of epochs
     broker1 = inverter.remove_broker(broker1)
 
     assert inverter.number_of_brokers() == 1
-    assert inverter.funds == 600
+    assert inverter.funds == 500
+    assert inverter.stake == 100
     assert broker1.funds == 100
 
     # Remove a broker while over the minimum number of epochs
@@ -133,7 +140,8 @@ def test_get_allocated_funds(inverter, broker1, broker2):
 
     broker2 = inverter.add_broker(broker2, 100)
 
-    assert inverter.funds == 700
+    assert inverter.funds == 500
+    assert inverter.stake == 200
     assert inverter.number_of_brokers() == 2
     assert inverter.get_allocated_funds() == 100
 
@@ -163,22 +171,23 @@ def test_cancel(owner, inverter, broker1, broker2):
     broker1 = inverter.add_broker(broker1, 50)
     broker2 = inverter.add_broker(broker2, 100)
     
-    # Check total funds: 500(owner initial amount) + 50 (broker1 stake) + 100 (broker2 stake)
-    assert inverter.funds == 650
+    # Check total funds: 500(owner initial amount), 150 from stakes
+    assert inverter.funds == 500
+    assert inverter.stake == 150
     
     inverter.iter_epoch(30)
     
     # Cancel the proposal inverter
     inverter.cancel(owner.public)
 
-    # Each broker makes their claim
-    broker1 = inverter.claim_broker_funds(broker1)
-    broker2 = inverter.claim_broker_funds(broker2)
+    # Each broker leaves the proposal
+    broker1 = inverter.remove_broker(broker1)
+    broker2 = inverter.remove_broker(broker2)
         
-    # Broker1 funds = 300 + 50(broker1's current funds)
+    # Broker1 funds = 50 (current funds) + 50 (stake) + 250 (claim) = 350
     assert broker1.funds == 350
     
-    # Broker2 funds = 350 + 0(broker2's current funds)
+    # Broker2 funds = 0 (current funds) + 100 (stake) + 250 (claim) = 350
     assert broker2.funds == 350
 
     # End state of proposal inverter
@@ -199,16 +208,19 @@ def test_forced_cancel_case1(broker1):
     
     # Add broker
     broker1 = inverter.add_broker(broker1, 9)
-
+    
     # Dip below the minimum conditions
     inverter.iter_epoch(5)
 
     broker1 = inverter.remove_broker(broker1)
+    print(inverter.broker_agreements)
 
     # Iterate past the buffer period
     inverter.iter_epoch(6)
 
-    assert inverter.number_of_brokers() < inverter.min_brokers
+    print(inverter.broker_agreements)
+
+    assert inverter.number_of_brokers() == 1
     assert inverter.get_horizon() < inverter.min_horizon
     assert inverter.get_allocated_funds() == inverter.funds
 
@@ -229,7 +241,8 @@ def test_forced_cancel_case2(broker1, broker2, payer):
     broker1 = inverter.add_broker(broker1, 10)
     broker2 = inverter.add_broker(broker2, 10)
 
-    assert inverter.funds == 120
+    assert inverter.funds == 100
+    assert inverter.stake == 20
     assert inverter.get_horizon() >= inverter.min_horizon
 
     # Dip below minimum conditions but before the forced cancel triggers
--- parameterized/test_wallet.py
+++ parameterized/test_wallet.py
@@ -26,34 +26,32 @@ def test_remove_proposal_inverter():
     broker1 = inverter.add_broker(broker1, 50)
     broker2 = inverter.add_broker(broker2, 100)
     
-    # Check total funds: 500(owner initial amount) + 50 (broker1 stake) + 100 (broker2 stake)
-    assert inverter.funds == 650
+    # Check total funds: 500(owner initial amount)
+    assert inverter.funds == 500
+    assert inverter.stake == 150
+    assert broker1.funds == 50
+    assert broker2.funds == 0
     
     inverter.iter_epoch(30)
     
-    # Create a prototype broker pool, which is a dictionary where the key is the broker public key
-    # and the value is the broker object
-    broker_pool = {
-        broker1.public: broker1,
-        broker2.public: broker2,
-    }
-    
     """
-    If we have a proposal inverter deployed at 500 and 2 brokers enter the agreement, 
-    broker 1 (stake=50) & broker2 (stake=100), enter at epoch=0, at epoch=30, 
-    each of their claimable funds would be 150. 
-    Broker 1 should receive: 50 + 150 + (650-1(150)-50-100)/2 = 50 + 150 +100 =300 
-    Broker 2 should receive: 100 + 150 + (650-1(150)-50-100)/2 = 50 + 150 +100 =350
+    If we have a proposal inverter deployed at 500 and 2 brokers enter the
+    agreement, broker 1 (stake=50) & broker2 (stake=100), enter at epoch=0, at
+    epoch=30, each of their claimable funds would be 150. 
+
+    Broker 1 should receive: 50 + 150 + (500 - (2 * 150)) / 2 = 300 
+    Broker 2 should receive: 100 + 150 + (500 - (2 * 150)) / 2 = 350
     """
     for public_key, broker_agreement in inverter.broker_agreements.items():
         assert broker_agreement.allocated_funds == 150
         
-    # Call the cancel function which returns the updated broker pool
-    broker_pool = owner.cancel(inverter, broker_pool)
+    # Call the cancel function and remove brokers
+    inverter.cancel(owner.public)
+    broker1 = inverter.remove_broker(broker1)
+    broker2 = inverter.remove_broker(broker2)
     
-    # Broker1 funds = 300 + 50(broker1's current funds)
+    # Broker1 funds = 300 + 50 (broker1's current funds)
     assert broker1.funds == 350
     
-    # Broker2 funds = 350 + 0(broker2's current funds)
+    # Broker2 funds = 350 + 0 (broker2's current funds)
     assert broker2.funds == 350
-
