88c1d996d6c86f6681721efa066a67907e98d3bb
MoatazAR <43458713+MoatazAR@users.noreply.github.com>
2021-12-09 16:52:31 -0800

    Update dogfood_model.py

--- parameterized/dogfood_model.py
+++ parameterized/dogfood_model.py
@@ -43,20 +43,21 @@ def payer():
     payer.funds = 100
 
     return payer
-def use_case_test1(owner, broker1, broker2, broker3, broker4, payer):
+
+def use_case_test(owner, broker1, broker2, broker3, broker4, payer):
     """
     This test is a practical use case scenario for the proposal inverter. It is meant to put
     this project into pragmatic scenario by using an example where the proposal inverter iself
     is a pitched proposal, the PrimoDAO team as the owners, and the token engineering team hired
-    to work on it as the brokers.
+    to work on it as the brokers. The pay period for the brokers is 15 epochs.
     """
     
     # Shawn (Broker1), with funding of PrimeDAO (owner), launches the proposal inverter as a project, secures initial funding and stakes 50
-    inverter = owner.deploy(500, broker_whitelist=NoVote())
+    inverter = owner.deploy(1000, broker_whitelist=NoVote())
     broker1 = inverter.add_broker(broker1, 50)
     
     # The current funds for the proposal inverter is the deployed amount plus Shawn's (Broker1's) stake
-    assert inverter.funds == 550
+    assert inverter.funds == 1050
     assert inverter.number_of_brokers() == 1
     
     # After a set amount of time, 2 new brokers, Taz (Broker2) and Austin(Broker3), are added to the proposal inverter (new hires)
@@ -65,18 +66,30 @@ def use_case_test1(owner, broker1, broker2, broker3, broker4, payer):
     broker3 = inverter.add_broker(broker3, 50)
     
     # After the addition of 2 brokers, the number of brokers increases, and the funds increase due to the additional stakes
-    assert inverter.funds == 650
+    assert inverter.funds == 1150
     assert inverter.number_of_brokers() == 3
     
     # After the first period elapses, the proposal is set for the next payment (either by an unknown invester/payer or PrimeDAO) if it's meeting expectations
+    # The brokers can claim funds for the epochs spent working on the proposal
     inverter.iter_epoch(5)
+    broker1 = inverter.claim_broker_funds(broker1)
+    broker2 = inverter.claim_broker_funds(broker2)
+    broker3 = inverter.claim_broker_funds(broker3)
     payer = inverter.pay(payer, 50)
-    assert inverter.funds == 700
+    assert inverter.funds == 1050
+    
+    # The brokers continue working on the proposal inverter research project & the proposal is approved for it's next pay period
+    inverter.iter_epoch(15)
+    broker1 = inverter.claim_broker_funds(broker1)
+    broker2 = inverter.claim_broker_funds(broker2)
+    broker3 = inverter.claim_broker_funds(broker3)
+    payer = inverter.pay(payer, 50)
+    assert inverter.funds == 950
     
     # After a set amount of time, new broker, Tomas (Broker3), joins the proposal
     inverter.iter_epoch(10)
     broker4 = inverter.add_broker(broker4, 50)
-    assert inverter.funds == 750
+    assert inverter.funds == 1000
     
     # After the proposal inverter project is complete, PrimeDAO closes the proposal
     inverter.cancel(owner.public)
